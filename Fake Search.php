71e3bed53d46a671c0f0ded1dded097f269fc5f8
MrDiaosi <gaxiha@126.com>
2017-02-10 23:34:37 -0600

    Add files via upload
    input something in search box, then it will go to google.

---
+++ Fake Search.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Fack Google</title>
+</head>
+<body>
+	<div align="center">
+		<form action="https://www.google.com/search">
+			<input type="text" name="q" />
+			<br />
+			<input type="submit" value="Search">
+
+		</form>
+	</div>
+</body>
+</html>
